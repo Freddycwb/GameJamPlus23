9193df739d5a284250f15337e228b4c2832d19d1
LeoGR22 <leonardogr2212@gmail.com>
2023-10-21 15:25:02 -0300

    Teleport

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Map : MonoBehaviour
+{
+   
+    public Transform player;
+
+    public void Teleport(Transform position)
+    {
+        player.position = position.position;
+    }
+}
