0d4d07fb93c7fcf86ab4b7ea0a0a261cec55edef
Freddy Matheus <45660002+Freddycwb@users.noreply.github.com>
2023-10-22 18:50:30 -0300

    Neighboor House 1 And 3

--- Assets/Scripts/DayNight.cs
+++ Assets/Scripts/DayNight.cs
@@ -25,7 +25,6 @@ public class DayNight : MonoBehaviour
     {
         for (int i = 0; i < nightNumber + 1; i++)
         {
-            Debug.Log("entrou e vai esperar " + nightTimer.timeToAction / (nightNumber + 2));
             yield return new WaitForSeconds(nightTimer.timeToAction / (nightNumber + 2));
             int spawnN = Random.Range(0, enemySpawners.Length);
             Instantiate(enemy, enemySpawners[spawnN].transform.position, transform.rotation);
